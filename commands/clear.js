7f4cf8be0b758962bd7382915682c4de24f61460
TP363 <80291244+TP363@users.noreply.github.com>
2021-03-15 18:33:34 +0330

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,9 +1,9 @@
 exports.run = async (bot,message,args) => {
-    if(!args[0]) return message.channel.send("please enter the amount of messages that you want to clear!");
-    if(isNaN(args[0])) return message.channel.send("please enter a real number!");
-    if(args[0] > 100) return message.channel.send("You can't delete more than 100 messages!");
+    if(!args[0]) return message.channel.send("please enter the amount of messages that you want to clear!")
+    if(isNaN(args[0])) return message.channel.send("please enter a real number!")
+    if(args[0] > 100) return message.channel.send("You can't delete more than 100 messages!")
 
-    if(args[0] < 1) return message.channel.send("You must delete at least one message!");
+    if(args[0] < 1) return message.channel.send("You must delete at least one message!")
 
     await message.channel.fetch({limit: args[0]}).then(messages =>{
         message.channel.bulkDelete(messages)
